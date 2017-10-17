7926fe1cc02ef44f6807c5ff7917efaebf74fa81
Canocles <rmm118@alu.ua.es>
2017-10-17 23:31:34 +0200

    Arreglado error en test

--- test/sg3-creacion-asociacion-tableros/ModeloRepositorioTableroTest.java
+++ test/sg3-creacion-asociacion-tableros/ModeloRepositorioTableroTest.java
@@ -92,10 +92,15 @@ public class ModeloRepositorioTableroTest {
 
   @Test
   public void testCrearTablaTableroEnBD() throws Exception {
+    Database db = injector.instanceOf(Database.class);
     Connection connection = db.getConnection();
     DatabaseMetaData meta = connection.getMetaData();
-    ResultSet res = meta.getTables(null, null, "TABLERO", new String[] {"TABLE"});
-    assertTrue(res.next());
+    // En la BD H2 el nombre de las tablas se define con mayúscula y en
+    // MySQL con minúscula
+    ResultSet resH2 = meta.getTables(null, null, "TABLERO", new String[] {"TABLE"});
+    ResultSet resMySQL = meta.getTables(null, null, "Tablero", new String[] {"TABLE"});
+    boolean existeTabla = resH2.next() || resMySQL.next();
+    assertTrue(existeTabla);
   }
 
   @Test
