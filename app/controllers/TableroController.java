694617991337c6b2e81ced70ea45e3ab420a0eeb
Canocles <rmm118@alu.ua.es>
2017-10-20 20:43:10 +0200

    Añadia acción y vista para añadir tableros administrados

--- app/controllers/TableroController.java
+++ app/controllers/TableroController.java
@@ -1,6 +1,10 @@
 package controllers;
 
 import play.mvc.*;
+import play.data.Form;
+import play.data.FormFactory;
+import play.data.DynamicForm;
+import play.Logger;
 
 import views.html.*;
 
@@ -17,8 +21,40 @@ import services.UsuarioService;
 
 public class TableroController extends Controller {
 
+  @Inject FormFactory formFactory;
   @Inject UsuarioService usuarioService;
   @Inject TableroService tableroService;
+
+  @Security.Authenticated(ActionAuthenticator.class)
+  public Result formNuevoTablero (Long idUsuario) {
+    String connectedUserStr = session("connected");
+    Long connectedUser =  Long.valueOf(connectedUserStr);
+    if (connectedUser != idUsuario) {
+      return unauthorized("Lo siento, no estás autorizado");
+    } else {
+      Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
+      return ok(formNuevoTablero.render(usuario, formFactory.form(Tablero.class),""));
+    }
+  }
+
+  @Security.Authenticated(ActionAuthenticator.class)
+  public Result crearNuevoTablero(Long idUsuario) {
+    String connectedUserStr = session("connected");
+    Long connectedUser =  Long.valueOf(connectedUserStr);
+    if (connectedUser != idUsuario) {
+      return unauthorized("Lo siento, no estás autorizado");
+    } else {
+      Form<Tablero> tableroForm = formFactory.form(Tablero.class).bindFromRequest();
+      if (tableroForm.hasErrors()) {
+        Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
+        return badRequest(formNuevoTablero.render(usuario, formFactory.form(Tablero.class), "Hay errores en el formulario"));
+      }
+      Tablero tablero = tableroForm.get();
+      tableroService.crearTableroUsuario(tablero.getNombre(), idUsuario);
+      flash("aviso", "El tablero se ha guardado correctamente");
+      return redirect(controllers.routes.TableroController.listarTablerosAdministrados(idUsuario));
+    }
+  }
 
   @Security.Authenticated(ActionAuthenticator.class)
   public Result listarTablerosAdministrados(Long idUsuario) {
